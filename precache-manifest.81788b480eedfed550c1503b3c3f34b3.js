1bcf534c5bb432927e2b531e694bd507b77f3d3e
Mai Banh <65269951+mtbanh@users.noreply.github.com>
2020-08-19 23:18:38 -0700

    Updates

---
+++ precache-manifest.81788b480eedfed550c1503b3c3f34b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d32e5fd033d2a99d034562f828e2052f",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "a1fb65c2b12c7acf8021",
+    "url": "/React-Portfolio/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "eb6b34c66e2a745dcdaf",
+    "url": "/React-Portfolio/static/js/2.ac3dc4c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Portfolio/static/js/2.ac3dc4c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1fb65c2b12c7acf8021",
+    "url": "/React-Portfolio/static/js/main.824e5049.chunk.js"
+  },
+  {
+    "revision": "69cfd8c03a1dcd4ed94e",
+    "url": "/React-Portfolio/static/js/runtime-main.2850e5db.js"
+  }
+]);
