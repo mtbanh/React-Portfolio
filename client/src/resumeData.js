0cee9e4205513f8bf6268cda6b2a958622928444
Mai Banh <65269951+mtbanh@users.noreply.github.com>
2020-11-10 14:59:40 -0800

    updating portfolio

--- client/src/resumeData.js
+++ client/src/resumeData.js
@@ -15,11 +15,19 @@ let resumeData = {
         },
  
     ], 
-     "aboutme": " UC Berkeley Full Stack Web Development Bootcamp Graduate",
-     "resume" : "https://docs.google.com/document/d/1KAqYa_4fjjXm_Rk5CpDK68VE2UQrI1cJfDUHNsdCHVw/edit?usp=sharing",
+     "aboutme": "Looking to join a team ",
+     "resume" : "https://docs.google.com/document/d/1wTSMlZnfGjRMKBiYBHhpKWQW0G6HdgUHtHLxyj3dim4/edit?usp=sharing",
      "address" : "Bay Area San Francisco",
      "email" : "banhtmai@gmail.com",
     "portfolio":[
+      {
+        "name":"FriendZone",
+        "description":" A full stack app emulating BumbleBFF- allows users to create profile, match with others, and start chatting",
+        "imgurl":"images/portfolio/Clicky-Game.gif",
+        "github" : "fa fa-github",
+        "url" : "https://github.com/mtbanh/Friend-Zone",
+        "livesite" : "https://desolate-headland-76764.herokuapp.com/"
+      },
       {
         "name":"Clicky Game",
         "description":"A memory game created with React",
@@ -29,45 +37,53 @@ let resumeData = {
         "livesite" : "https://mtbanh.github.io/Clicky-Game/"
       },
       {
-        "name":"Employee Tracker",
-        "description":"Utilizing React to create an employee directory that allow users to view, and sort their employee databse",
+        "name":"Reading List",
+        "description":"A MERN application utilizing Google Books API to allow user to search and save books",
         "imgurl":"images/portfolio/Employee-Directory.gif",
         "github" : "fa fa-github",
-        "url" : "https://github.com/mtbanh/Employee-Directory",
-        "livesite" : "https://mtbanh.github.io/Employee-Directory/"
+        "url" : "https://github.com/mtbanh/search_book",
+        "livesite" : "https://nameless-hollows-64586.herokuapp.com/"
       },
-      {
-        "name":"ProjectX",
-        "description":"Full Stack application created with Express, mySQL and Passport for user authentication",  
-        "imgurl":"images/portfolio/ProjectX.gif",
-        "github" : "fa fa-github",
-        "url" : "https://github.com/mtbanh/ProjectX",
-        "livesite" : "https://fast-meadow-38464.herokuapp.com/"
-      },
-      {
-        "name":"Employee Tracker",
-        "description":"Utilizing CLI and mySQL to allow user to add, view and edit their employee database",
-        "imgurl":"images/portfolio/Employee-Tracker.gif",
-        "github" : "fa fa-github",
-        "url" : "https://github.com/mtbanh/Employee-Tracker",
-        // "livesite" : "https://mtbanh.github.io/Employee-Directory/"
-      },
-      {
-        "name":"hangryFixed",
-        "description":"Utilizing Yelp API and Googmaps API, this application will randomly generate a restaurant to eat at base on user input",
-        "imgurl":"images/portfolio/hangryFixed.gif",
-        "github" : "fa fa-github",
-        "url" : "https://github.com/mtbanh/hangryFixed",
-        "livesite" : "https://idelmundo.github.io/hangryfixed/"
-      },
-      {
-        "name":"Weather Dashboard",
-        "description":"Utilizing openWeather API and local storage, this application allow user to search for the forecast of a specific city",
-        "imgurl":"images/portfolio/weather-dashboard.gif",
-        "github" : "fa fa-github",
-        "url" : "https://github.com/mtbanh/",
-        "livesite" : "https://mtbanh.github.io/weather-dashboard/"
-      },
+      // {
+      //   "name":"Employee Tracker",
+      //   "description":"Utilizing React to create an employee directory that allow users to view, and sort their employee databse",
+      //   "imgurl":"images/portfolio/Employee-Directory.gif",
+      //   "github" : "fa fa-github",
+      //   "url" : "https://github.com/mtbanh/Employee-Directory",
+      //   "livesite" : "https://mtbanh.github.io/Employee-Directory/"
+      // },
+      // {
+      //   "name":"ProjectX",
+      //   "description":"Full Stack application created with Express, mySQL and Passport for user authentication",  
+      //   "imgurl":"images/portfolio/ProjectX.gif",
+      //   "github" : "fa fa-github",
+      //   "url" : "https://github.com/mtbanh/ProjectX",
+      //   "livesite" : "https://fast-meadow-38464.herokuapp.com/"
+      // },
+      // {
+      //   "name":"Employee Tracker",
+      //   "description":"Utilizing CLI and mySQL to allow user to add, view and edit their employee database",
+      //   "imgurl":"images/portfolio/Employee-Tracker.gif",
+      //   "github" : "fa fa-github",
+      //   "url" : "https://github.com/mtbanh/Employee-Tracker",
+      //   // "livesite" : "https://mtbanh.github.io/Employee-Directory/"
+      // },
+      // {
+      //   "name":"hangryFixed",
+      //   "description":"Utilizing Yelp API and Googmaps API, this application will randomly generate a restaurant to eat at base on user input",
+      //   "imgurl":"images/portfolio/hangryFixed.gif",
+      //   "github" : "fa fa-github",
+      //   "url" : "https://github.com/mtbanh/hangryFixed",
+      //   "livesite" : "https://idelmundo.github.io/hangryfixed/"
+      // },
+      // {
+      //   "name":"Weather Dashboard",
+      //   "description":"Utilizing openWeather API and local storage, this application allow user to search for the forecast of a specific city",
+      //   "imgurl":"images/portfolio/weather-dashboard.gif",
+      //   "github" : "fa fa-github",
+      //   "url" : "https://github.com/mtbanh/",
+      //   "livesite" : "https://mtbanh.github.io/weather-dashboard/"
+      // },
     ]
 }
   
